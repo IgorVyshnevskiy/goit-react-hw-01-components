c76e038e7abf4262df7c83f875c60333c580e33a
Igor Vyshnevskyi <igor.vyshnevskiy097@gmail.com>
2023-09-16 17:29:59 +0300

    searching problem with json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,29 @@
 import user from '../components/Profile/user.json';
-import data from '../components/Statistics/data.json'
-import friends from '../components/FriendsList/friends.json'
+import data from '../components/Statistics/data.json';
+import friends from '../components/FriendsList/friends.json';
+import transactions from '../components/Transactions/transactions.json'
 
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendsList/FriendsList';
-
-
-
+import TransactionHistory from './Transactions/Transactions';
 
 export const App = () => {
   return (
-   <section>
-    <Profile 
-      imageUrl={user.avatar}
-      userName={user.username}
-      userTag={user.tag}
-      userLocation={user.location}
-      stats={user.stats}
-    />
+    <section>
+      <Profile
+        imageUrl={user.avatar}
+        userName={user.username}
+        userTag={user.tag}
+        userLocation={user.location}
+        stats={user.stats}
+      />
 
-    <Statistics title={'Upload stats'} stats={data} />
+      <Statistics title={'Upload stats'} stats={data} />
 
-    <FriendList
-      friends={friends}
-    />
-  </section>
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions}/>   
+    </section>
   );
 };
